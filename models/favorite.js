184a579bbdc8ee45bea2b8b2e6e488069cebd99c
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-06-23 12:26:45 -0700

    Workshop week 4

---
+++ models/favorite.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const favoriteSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  campsites: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Campsite",
+    },
+  ],
+});
+
+const Favorite = mongoose.model("Favorite", favoriteSchema);
+
+module.exports = Favorite;
